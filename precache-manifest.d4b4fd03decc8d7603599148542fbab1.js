ea192e14746cd94fd9f634e95bc4200ab0921b27
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-12-26 12:10:35 -0800

    Updates

---
+++ precache-manifest.d4b4fd03decc8d7603599148542fbab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45638ecb45d91e2467f29329c9476fe",
+    "url": "/the-movie-db/build/index.html"
+  },
+  {
+    "revision": "62130808b884195e960b",
+    "url": "/the-movie-db/build/static/css/main.91675ea4.chunk.css"
+  },
+  {
+    "revision": "8089a86d4679aba30069",
+    "url": "/the-movie-db/build/static/js/2.6290126b.chunk.js"
+  },
+  {
+    "revision": "62130808b884195e960b",
+    "url": "/the-movie-db/build/static/js/main.17d981a6.chunk.js"
+  },
+  {
+    "revision": "102792c9162bf6d8b261",
+    "url": "/the-movie-db/build/static/js/runtime-main.0b21b5fc.js"
+  },
+  {
+    "revision": "03b7dd2cbfc42f9570409abf288a9401",
+    "url": "/the-movie-db/build/static/media/spinner.03b7dd2c.gif"
+  }
+]);
