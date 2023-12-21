097eb3f67f4e1c47d2bdbc3d32498e9b2e4ed2dc
Kyle Trinh <kyle.trinh94@gmail.com>
2023-12-20 17:33:44 -0800

    :lock: Move API Key to .env file

--- src/actions/multi.js
+++ src/actions/multi.js
@@ -8,11 +8,11 @@ import {
   GET_DETAIL_MOVIES,
   GET_DETAIL_MOVIES_ERROR,
   SEARCH_MOVIES,
-  SEARCH_MOVIES_ERROR
+  SEARCH_MOVIES_ERROR,
 } from '../constants';
 
 export function getMovieByGenre(genreId, page = 1, mediaType = MOVIE) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     try {
       const res = await axios.get(
         `${PATH_BASE}/discover/${mediaType}?api_key=${API_KEY}&language=en-US&with_genres=${genreId}&page=${page}`
@@ -20,19 +20,19 @@ export function getMovieByGenre(genreId, page = 1, mediaType = MOVIE) {
 
       dispatch({
         type: GET_MOVIES_BY_GENRE,
-        payload: res.data
+        payload: res.data,
       });
     } catch (err) {
       dispatch({
         type: GET_MOVIES_BY_GENRE_ERROR,
-        payload: err.response.data.status_message
+        payload: err.response.data.status_message,
       });
     }
   };
 }
 
 export function getMovieDetails(id, mediaType = MOVIE) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     try {
       const res = await axios.get(
         `${PATH_BASE}/${mediaType}/${id}?api_key=${API_KEY}&language=en-US`
@@ -40,32 +40,32 @@ export function getMovieDetails(id, mediaType = MOVIE) {
 
       dispatch({
         type: GET_DETAIL_MOVIES,
-        payload: res.data
+        payload: res.data,
       });
     } catch (err) {
       dispatch({
         type: GET_DETAIL_MOVIES_ERROR,
-        payload: err.response.data.status_message
+        payload: err.response.data.status_message,
       });
     }
   };
 }
 
 export function searchMovies(query) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     try {
       const res = await axios.get(
-        `${PATH_BASE}/search/multi/?api_key=${API_KEY}&query=${query}`
+        `${PATH_BASE}/search/multi?api_key=${API_KEY}&query=${query}`
       );
 
       dispatch({
         type: SEARCH_MOVIES,
-        payload: res.data.results
+        payload: res.data.results,
       });
     } catch (err) {
       dispatch({
         type: SEARCH_MOVIES_ERROR,
-        payload: err.response.data.status_message
+        payload: err.response.data.status_message,
       });
     }
   };
--- src/constants/movieDB.js
+++ src/constants/movieDB.js
@@ -1,4 +1,5 @@
-const API_KEY = '3cd96ee899938e2296da3270c1887d0b';
+// const API_KEY = '3cd96ee899938e2296da3270c1887d0b';
+const API_KEY = process.env.REACT_APP_API_KEY;
 const PATH_BASE = 'https://api.themoviedb.org/3';
 
 export { API_KEY, PATH_BASE };
